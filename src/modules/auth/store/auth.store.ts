c6edbfa10aa88be9938c7efda4aea2a60c0ea545
Sayed Yousuf Mahbobi <rayanguzman345@gmail.com>
2026-03-25 21:54:21 +0100

    feat(auth): add authentication state management with Pinia

---
+++ src/modules/auth/store/auth.store.ts
@@ -0,0 +1,69 @@
+import { defineStore } from "pinia";
+import {
+  getCsrfCookie,
+  getLoggedInUser,
+  login,
+  logout,
+} from '../api/auth.api';
+import type { LoginPayload } from '../schemas/auth.schema';
+import type { User } from "../types/auth.types";
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null as User | null,
+    loading: false,
+    initialized: false,
+  }),
+
+  getters:{
+    isAuthenticated: (state) => !!state.user,
+  },
+
+  actions: {
+    /**
+     * Initialize auth state on app start
+     */
+    async initialize() {
+      if (this.initialized) return
+
+      try {
+        const response = await getLoggedInUser()
+        this.user = response.data.data
+      } catch {
+        this.user = null
+      } finally {
+        this.initialized = true
+      }
+    },
+
+    /**
+     * Login user
+     */
+    async login(payload: LoginPayload) {
+      if (this.loading) return
+
+      this.loading = true
+
+      try {
+        await getCsrfCookie()
+        await login(payload)
+
+        // Option 1: fetch fresh user (recommended)
+        await this.initialize()
+
+      } finally {
+        this.loading = false
+      }
+    },
+
+    /**
+     * Logout user
+     */
+    async logout() {
+      await logout()
+      this.user = null
+      this.initialized = false
+    },
+  },
+
+})
