31873fff2d9450e35dc4504ad9669dab795badd2
Sayed Yousuf Mahbobi <rayanguzman345@gmail.com>
2026-03-26 15:29:32 +0100

    feat(router): define route structure with protected and guest-only layouts

--- src/plugins/router/routes.ts
+++ src/plugins/router/routes.ts
@@ -1,5 +1,10 @@
 export const routes = [
-  { path: '/', redirect: '/dashboard' },
+  {
+    path: '/',
+    redirect: '/dashboard',
+  },
+
+  // Protected routes
   {
     path: '/',
     component: () => import('@/layouts/default.vue'),
@@ -7,17 +12,31 @@ export const routes = [
       {
         path: 'dashboard',
         component: () => import('@/pages/dashboard.vue'),
+        name: 'dashboard',
+        meta: { requiresAuth: true },
       },
     ],
   },
+
+  // Guest-only routes
   {
     path: '/',
     component: () => import('@/layouts/blank.vue'),
     children: [
       {
-        path: '/:pathMatch(.*)*',
-        component: () => import('@/pages/[...error].vue'),
+        path: 'login',
+        name: 'login',
+        component: () => import('@/pages/login.vue'),
+        meta: { guestOnly: true },
       },
     ],
   },
+
+  // Catch-all route for 404 Not Found
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: () => import('@/pages/[...error].vue'),
+  },
+
 ]
