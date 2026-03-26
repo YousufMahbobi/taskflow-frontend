adf63f855a8964a022f42bec1b8e3acf00c31cd3
Sayed Yousuf Mahbobi <rayanguzman345@gmail.com>
2026-03-26 16:09:29 +0100

    refactor(auth): simplify Materio login page for custom implementation

---
+++ src/pages/login.vue
@@ -0,0 +1,119 @@
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+
+import logo from '@images/logo.png'
+import authV1MaskDark from '@images/pages/auth-v1-mask-dark.png'
+import authV1MaskLight from '@images/pages/auth-v1-mask-light.png'
+import authV1Tree2 from '@images/pages/auth-v1-tree-2.png'
+import authV1Tree from '@images/pages/auth-v1-tree.png'
+
+const form = ref({
+  email: '',
+  password: '',
+  remember: false,
+})
+
+const vuetifyTheme = useTheme()
+
+const authThemeMask = computed(() => {
+  return vuetifyTheme.global.name.value === 'light'
+    ? authV1MaskLight
+    : authV1MaskDark
+})
+
+const isPasswordVisible = ref(false)
+</script>
+
+<template>
+  <!-- eslint-disable vue/no-v-html -->
+
+  <div class="auth-wrapper d-flex align-center justify-center pa-4">
+    <VCard
+      class="auth-card pa-4 pt-7"
+      max-width="448"
+    >
+      <VCardItem class="justify-center">
+          <VImg
+            :src="logo"
+            alt="logo"
+            class="d-flex w-100"
+          />
+      
+          <h2 class="font-weight-medium text-2xl text-uppercase">
+            International
+          </h2>
+      </VCardItem>
+
+      <VCardText class="pt-2">
+        <h4 class="text-h4 mb-1">
+          Welcome to Axios Portal!
+        </h4>
+        <p class="mb-0">
+          Please login-in to your account and start the adventure
+        </p>
+      </VCardText>
+
+      <VCardText>
+        <VForm @submit.prevent="() => {}">
+          <VRow>
+            <!-- email -->
+            <VCol cols="12">
+              <VTextField
+                v-model="form.email"
+                label="Email"
+                type="email"
+              />
+            </VCol>
+
+            <!-- password -->
+            <VCol cols="12">
+              <VTextField
+                v-model="form.password"
+                label="Password"
+                placeholder="············"
+                :type="isPasswordVisible ? 'text' : 'password'"
+                autocomplete="password"
+                :append-inner-icon="isPasswordVisible ? 'ri-eye-off-line' : 'ri-eye-line'"
+                @click:append-inner="isPasswordVisible = !isPasswordVisible"
+              />
+            </VCol>
+
+            <!-- login button -->
+            <VCol cols="12">
+              <VBtn
+                block
+                type="submit"
+                to="/"
+              >
+                Login
+              </VBtn>
+           </VCol>
+
+          </VRow>
+        </VForm>
+      </VCardText>
+    </VCard>
+
+    <VImg
+      class="auth-footer-start-tree d-none d-md-block"
+      :src="authV1Tree"
+      :width="250"
+    />
+
+    <VImg
+      :src="authV1Tree2"
+      class="auth-footer-end-tree d-none d-md-block"
+      :width="350"
+    />
+
+    <!-- bg img -->
+    <VImg
+      class="auth-footer-mask d-none d-md-block"
+      :src="authThemeMask"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+@use "@core/scss/template/pages/page-auth";
+</style>
